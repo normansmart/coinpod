bc6eb2a77b8755c3a6797daa9c173b9a6326d15c
Norman Smart <normansmart14@gmail.com>
2022-08-05 10:38:22 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -6,29 +6,29 @@ import SigninPage from './components/SigninPage';
 import LoginForm from './components/LoginForm';
 import DashBoard from './components/DashBoard';
 import PodsPage from './components/PodsPage';
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
 
   const [currentUserId, setCurrentUserId] = useState(null)
-  const [users , setUsers] = useState([])
-  const [banks , setBanks] = useState([])
-  const [goals , setGoals] = useState([])
-  const [commits , setCommits] = useState([])
+  const [users, setUsers] = useState([])
+  const [banks, setBanks] = useState([])
+  const [goals, setGoals] = useState([])
+  const [commits, setCommits] = useState([])
 
   useEffect(() => {
     fetch("/me")
-    .then(r => r.json())
-    .then((user) => {
-      if (user.id) {
-        setCurrentUserId(user.id)
-        setUsers(user)
-        setBanks(user.banks)
-        setGoals(user.goals)
-        setCommits(user.commits)
-      }
-    })
+      .then(r => r.json())
+      .then((user) => {
+        if (user.id) {
+          setCurrentUserId(user.id)
+          setUsers(user)
+          setBanks(user.banks)
+          setGoals(user.goals)
+          setCommits(user.commits)
+        }
+      })
   }, [])
 
   function onLogin(userId) {
@@ -36,52 +36,38 @@ function App() {
     console.log(`logged in user ${userId}`)
 
     fetch("/me")
-    .then(r => r.json())
-    .then( user => {
-      setUsers(user)
-      setBanks(user.banks)
-      setGoals(user.goals)
-      setCommits(user.commits)
-    })
+      .then(r => r.json())
+      .then(user => {
+        setUsers(user)
+        setBanks(user.banks)
+        setGoals(user.goals)
+        setCommits(user.commits)
+      })
   }
 
-<<<<<<< HEAD
-  })
-
-  
-} , [])
-
-
-
-
-
-
-=======
-  function logout() {
-    console.log('logging out')
-    fetch("/logout", {
-      method: "DELETE"
-    })
-    setCurrentUserId(null)
-  }
->>>>>>> refs/remotes/origin/main
-
-
-  return (
-    <div>
-      <div className="App">
-        <Navbar currentUserId={currentUserId} logout={logout}/>
-      </div>
-      <div>
-        <SignupForm onLogin={onLogin} currentUserId={currentUserId}/>
-        <LoginForm onLogin={onLogin} currentUserId={currentUserId}/>
-        {/* <SigninPage /> */}
-
-        {/*  <DashBoard user={Users} banks={Banks} goals={Goals} /> */}
-        <PodsPage user={users} banks={banks} goals={goals} />
-      </div>
-    </div>
-  );
 }
+
+
+
+
+
+
+
+
+return (
+  <div>
+    <div className="App">
+      <Navbar currentUserId={currentUserId} logout={logout} />
+    </div>
+    <div>
+      <SignupForm onLogin={onLogin} currentUserId={currentUserId} />
+      <LoginForm onLogin={onLogin} currentUserId={currentUserId} />
+      {/* <SigninPage /> */}
+
+      {/*  <DashBoard user={Users} banks={Banks} goals={Goals} /> */}
+      <PodsPage user={users} banks={banks} goals={goals} />
+    </div>
+  </div>
+);
 
 export default App;
